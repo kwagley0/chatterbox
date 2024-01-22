3c9aef285f8448731c899acf96292f26a0020c10
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-01-22 17:35:41 -0500

    Fix 6

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,28 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-const colors = require("colors");
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const path = require("path");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
+const colors = require("colors");
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API is Running Successfully");
-});
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-//Deployment
+// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -39,8 +38,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-//Deployment
+// --------------------------deployment------------------------------
 
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
@@ -48,19 +48,19 @@ const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
-  console.log(`Server started on PORT ${PORT}`.yellow.bold)
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
+    // credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
-
   socket.on("setup", (userData) => {
     socket.join(userData._id);
     socket.emit("connected");
@@ -70,19 +70,18 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
   });
-
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageReceived) => {
-    var chat = newMessageReceived.chat;
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
 
     if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
-      if (user._id == newMessageReceived.sender._id) return;
+      if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit("message received", newMessageReceived);
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
 
